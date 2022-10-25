585c049757642c9f80fa48beea1488b10757ef27
Kenneth Onu <kennetho.onu@gmail.com>
2022-10-26 02:55:32 +0800

    third commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const minsEl = document.getElementById('minutes')
 const secsEl = document.getElementById('seconds')
 const ampmEl = document.getElementById('ampm')
 const timeOfDayEl = document.getElementById('timeOfDay')
-const dayEl = document.getElementById('day')
+const dateEl = document.getElementById('day')
 const monthEl = document.getElementById('month')
 const yearEl = document.getElementById('year')
 const seasonEl = document.getElementById('season')
@@ -13,7 +13,7 @@ function updateCalendar(){
 	let mins = new Date().getMinutes()
 	let secs = new Date().getSeconds()
 	let ampm = 'AM'
-	let day = new Date().getDate()
+	let date = new Date().getDate()
 	let month = new Date().getMonth() + 1
 	let year = new Date().getFullYear()
 	let season = 'RAINY'
@@ -27,19 +27,19 @@ function updateCalendar(){
 	hr = hr < 10 ? '0' + hr : hr 
 	mins = mins < 10 ? '0' + mins : mins 
 	secs = secs < 10 ? '0' + secs : secs
-	day = day < 10 ? '0' + day : day
+	date = date < 10 ? '0' + date : date
+	month = month < 10 ? '0' + month : month
 	season = month < 3 ? 'dry' : 
 			 month > 3 && month < 6 ? 'rainy' : 
 			 month > 6 && month < 9 ? 'hot'	: 
 			 'cold'
-	month = month < 10 ? '0' + month : month
 
 	hourEl.innerText = hr
 	minsEl.innerText = mins 
 	secsEl.innerText = secs
 	ampmEl.innerText = ampm
 	timeOfDayEl.innerText = timeOfDay 
-	dayEl.innerText = day
+	dateEl.innerText = date
 	monthEl.innerText = month 
 	yearEl.innerText = year
 	seasonEl.innerText = season 
